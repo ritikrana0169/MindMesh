212a8e95f0c3f45ed4abc8eec306c3e2d5304dd2
Pranay <pmcanvas4501@gmail.com>
2023-06-24 18:55:30 +0530

    reports generation work

--- BackendNode/index.js
+++ BackendNode/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
 app.use(express.json())
+
 const { exerciseRouter } = require("./routes/exercise.routes")
 
 app.use("/exercise",exerciseRouter)
--- BackendNode/routes/exercise.routes.js
+++ BackendNode/routes/exercise.routes.js
@@ -1,9 +1,11 @@
 const express=require("express");
-const { getQuestions, getCompare } = require("../controllers/exercise.controllers");
+const { getQuestions, getCompare, saveReport } = require("../controllers/exercise.controllers");
 const exerciseRouter=express.Router()
 
 exerciseRouter.post("/getquestions",getQuestions)
 
 exerciseRouter.post("/compare",getCompare)
+
+exerciseRouter.post("/save-report",saveReport)
 
 module.exports={exerciseRouter};
