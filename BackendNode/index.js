f317bc794a1fb4ff46263487d3cb59eb0744524f
sagardeswal123 <sagardeswal798@gmail.com>
2023-06-24 20:02:02 +0530

    cors added

--- BackendNode/index.js
+++ BackendNode/index.js
@@ -1,9 +1,12 @@
 const express=require("express")
 const app=express()
+const cors = require("cors");
 app.use(express.json())
 const { exerciseRouter } = require("./routes/exercise.routes")
 
-app.use("/exercise",exerciseRouter)
+app.use(cors());
+
+app.use("/exercise",exerciseRouter);
 
 app.listen(7500,()=>{
     console.log("server running at 7500")
